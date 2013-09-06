8c64499cffa4b1b02a1feb0f43bfb65eeeb075cd
SpinyLobster <spinylobster@hotmail.co.jp>
2013-09-07 08:20:26 +0900

    Initial Commit.

---
+++ MyTai_eclipse/src/jp/jagaimo/mytai/MainActivity.java
@@ -0,0 +1,30 @@
+package jp.jagaimo.mytai;
+
+import jp.jagaimo.utils.App;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		App.Initialize(this);
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		App.SetTransition(R.id.start_button, SettingsActivity.class, this);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+
+
+}
